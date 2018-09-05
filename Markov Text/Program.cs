4c3685c8f46bac26fc65c27ecadabbdbe749c1da
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-09-05 07:50:18 -0400

    Add project files.

---
+++ Markov Text/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
